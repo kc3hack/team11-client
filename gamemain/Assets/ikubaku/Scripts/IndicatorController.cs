3295da7973f3a297b1ccaad50d5ade608adf28ee
ikubaku <hide4d51@gmail.com>
2019-02-19 20:13:55 +0900

    Update spawner

---
+++ gamemain/Assets/ikubaku/Scripts/IndicatorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorController : MonoBehaviour {
+    public Sprite SpriteOnActive;
+    public Sprite SpriteOnInactive;
+
+    private SpriteRenderer sr;
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeToActive()
+    {
+        sr.sprite = SpriteOnActive;
+    }
+    public void ChangeToInactive()
+    {
+        sr.sprite = SpriteOnInactive;
+    }
+}
--- gamemain/Assets/ikubaku/Scripts/TatekanSpawnerController.cs
+++ gamemain/Assets/ikubaku/Scripts/TatekanSpawnerController.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TatekanSpawnerController : MonoBehaviour {
-    public GameObject TatekanBig;
-    public GameObject TatekanMiddle;
-    public GameObject TatekanSmall;
+    public GameObject[] TatekansBig;
+    public GameObject[] TatekansMiddle;
+    public GameObject[] TatekansSmall;
+
+    public float BigCooldown = 10f;
+    public float MiddleCooldown = 5f;
+    public float SmallCooldown = 3f;
+    public IndicatorController BigIndicator;
+    public IndicatorController MiddleIndicator;
+    public IndicatorController SmallIndicator;
 
     public GameObject[] SpawnTargets;
-    public TatekanController TatekanMController;
+    public float SpawnSpreadRange = 1f;
 
+    private GameObject TatekanBig;
+    private GameObject TatekanMiddle;
+    private GameObject TatekanSmall;
     private int spawn_idx = 0;
     private bool is_fire1_pressed = false;
     private bool is_fire2_pressed = false;
     private bool is_fire3_pressed = false;
+    private float cnt_big_cooldown = 0f;
+    private float cnt_middle_cooldown = 0f;
+    private float cnt_small_cooldown = 0f;
+    private Vector3 ofs_spread = Vector3.zero;
+
+
+    Vector3 get_random_vector(float r)
+    {
+        return new Vector3(Mathf.Cos(r), Mathf.Sin(r), 0f) * SpawnSpreadRange;
+    }
 
 	// Use this for initialization
 	void Start () {
@@ -25,16 +45,21 @@ public class TatekanSpawnerController : MonoBehaviour {
 		if(!is_fire1_pressed && Input.GetAxis("Fire1") > 0f)
         {
             is_fire1_pressed = true;
-            if (!TatekanMController.IsFull())
+            if (cnt_big_cooldown <= 0f)
             {
+                cnt_big_cooldown = BigCooldown;
+                BigIndicator.ChangeToInactive();
+
+                TatekanBig = TatekansBig[Mathf.FloorToInt(Random.Range(0, TatekansBig.Length))];
+
                 GameObject new_tatekan = Instantiate(TatekanBig);
-                new_tatekan.transform.position = SpawnTargets[spawn_idx].transform.position;
+                new_tatekan.transform.position = SpawnTargets[spawn_idx].transform.position + ofs_spread;
                 spawn_idx++;
                 if (spawn_idx >= SpawnTargets.Length)
                 {
                     spawn_idx = 0;
+                    ofs_spread = get_random_vector(Random.Range(0f, 2 * Mathf.PI));
                 }
-                TatekanMController.IncreaseTatekan();
             }
         }
         if(Input.GetAxis("Fire1") <= 0f)
@@ -45,16 +70,21 @@ public class TatekanSpawnerController : MonoBehaviour {
         if (!is_fire2_pressed && Input.GetAxis("Fire2") > 0f)
         {
             is_fire2_pressed = true;
-            if (!TatekanMController.IsFull())
+            if (cnt_middle_cooldown <= 0f)
             {
+                cnt_middle_cooldown = MiddleCooldown;
+                MiddleIndicator.ChangeToInactive();
+
+                TatekanMiddle = TatekansMiddle[Mathf.FloorToInt(Random.Range(0, TatekansMiddle.Length))];
+
                 GameObject new_tatekan = Instantiate(TatekanMiddle);
-                new_tatekan.transform.position = SpawnTargets[spawn_idx].transform.position;
+                new_tatekan.transform.position = SpawnTargets[spawn_idx].transform.position + ofs_spread;
                 spawn_idx++;
                 if (spawn_idx >= SpawnTargets.Length)
                 {
                     spawn_idx = 0;
+                    ofs_spread = get_random_vector(Random.Range(0f, 2 * Mathf.PI));
                 }
-                TatekanMController.IncreaseTatekan();
             }
         }
         if (Input.GetAxis("Fire2") <= 0f)
@@ -65,21 +95,52 @@ public class TatekanSpawnerController : MonoBehaviour {
         if (!is_fire3_pressed && Input.GetAxis("Fire3") > 0f)
         {
             is_fire3_pressed = true;
-            if (!TatekanMController.IsFull())
+            if (cnt_small_cooldown <= 0f)
             {
+                cnt_small_cooldown = SmallCooldown;
+                SmallIndicator.ChangeToInactive();
+
+                TatekanSmall = TatekansSmall[Mathf.FloorToInt(Random.Range(0, TatekansSmall.Length))];
+
                 GameObject new_tatekan = Instantiate(TatekanSmall);
-                new_tatekan.transform.position = SpawnTargets[spawn_idx].transform.position;
+                new_tatekan.transform.position = SpawnTargets[spawn_idx].transform.position + ofs_spread;
                 spawn_idx++;
                 if (spawn_idx >= SpawnTargets.Length)
                 {
                     spawn_idx = 0;
+                    ofs_spread = get_random_vector(Random.Range(0f, 2 * Mathf.PI));
                 }
-                TatekanMController.IncreaseTatekan();
             }
         }
         if (Input.GetAxis("Fire3") <= 0f)
         {
             is_fire3_pressed = false;
+        }
+
+        // Cooldown
+        if(cnt_big_cooldown > 0f)
+        {
+            cnt_big_cooldown -= Time.deltaTime;
+            if(cnt_big_cooldown <= 0f)
+            {
+                BigIndicator.ChangeToActive();
+            }
+        }
+        if(cnt_middle_cooldown > 0f)
+        {
+            cnt_middle_cooldown -= Time.deltaTime;
+            if(cnt_middle_cooldown <= 0f)
+            {
+                MiddleIndicator.ChangeToActive();
+            }
+        }
+        if(cnt_small_cooldown > 0f)
+        {
+            cnt_small_cooldown -= Time.deltaTime;
+            if(cnt_small_cooldown <= 0f)
+            {
+                SmallIndicator.ChangeToActive();
+            }
         }
     }
 }
