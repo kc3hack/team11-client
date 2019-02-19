5b1402ac0d2b88d164740c7514aa33a553d35a6f
ikubaku <hide4d51@gmail.com>
2019-02-19 17:09:16 +0900

    Add the tatekan spawnter

--- gamemain/Assets/ikubaku/Scripts/TatekanSpawnerController.cs
+++ gamemain/Assets/ikubaku/Scripts/TatekanSpawnerController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TatekanSpawnerController : MonoBehaviour {
+    public GameObject TatekanBig;
+    public GameObject[] SpawnTargets;
+
+    private int spawn_idx = 0;
+    private bool is_fire1_pressed = false;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +16,20 @@ public class TatekanSpawnerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(!is_fire1_pressed && Input.GetAxis("Fire1") > 0f)
+        {
+            is_fire1_pressed = true;
+            GameObject new_tatekan = Instantiate(TatekanBig);
+            new_tatekan.transform.position = SpawnTargets[spawn_idx].transform.position;
+            spawn_idx++;
+            if(spawn_idx >= SpawnTargets.Length)
+            {
+                spawn_idx = 0;
+            }
+        }
+        if(Input.GetAxis("Fire1") <= 0f)
+        {
+            is_fire1_pressed = false;
+        }
 	}
 }
