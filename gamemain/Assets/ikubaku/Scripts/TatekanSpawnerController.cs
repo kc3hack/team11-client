17daa7e7bcebc566215d8315ae6964c5bb34f49f
ikubaku <hide4d51@gmail.com>
2019-02-19 17:33:44 +0900

    Add size variants for tatekans

--- gamemain/Assets/ikubaku/Scripts/TatekanSpawnerController.cs
+++ gamemain/Assets/ikubaku/Scripts/TatekanSpawnerController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class TatekanSpawnerController : MonoBehaviour {
     public GameObject TatekanBig;
+    public GameObject TatekanMiddle;
+    public GameObject TatekanSmall;
+
     public GameObject[] SpawnTargets;
     public TatekanController TatekanMController;
 
     private int spawn_idx = 0;
     private bool is_fire1_pressed = false;
+    private bool is_fire2_pressed = false;
+    private bool is_fire3_pressed = false;
 
 	// Use this for initialization
 	void Start () {
@@ -36,5 +41,45 @@ public class TatekanSpawnerController : MonoBehaviour {
         {
             is_fire1_pressed = false;
         }
-	}
+
+        if (!is_fire2_pressed && Input.GetAxis("Fire2") > 0f)
+        {
+            is_fire2_pressed = true;
+            if (!TatekanMController.IsFull())
+            {
+                GameObject new_tatekan = Instantiate(TatekanMiddle);
+                new_tatekan.transform.position = SpawnTargets[spawn_idx].transform.position;
+                spawn_idx++;
+                if (spawn_idx >= SpawnTargets.Length)
+                {
+                    spawn_idx = 0;
+                }
+                TatekanMController.IncreaseTatekan();
+            }
+        }
+        if (Input.GetAxis("Fire2") <= 0f)
+        {
+            is_fire2_pressed = false;
+        }
+
+        if (!is_fire3_pressed && Input.GetAxis("Fire3") > 0f)
+        {
+            is_fire3_pressed = true;
+            if (!TatekanMController.IsFull())
+            {
+                GameObject new_tatekan = Instantiate(TatekanSmall);
+                new_tatekan.transform.position = SpawnTargets[spawn_idx].transform.position;
+                spawn_idx++;
+                if (spawn_idx >= SpawnTargets.Length)
+                {
+                    spawn_idx = 0;
+                }
+                TatekanMController.IncreaseTatekan();
+            }
+        }
+        if (Input.GetAxis("Fire3") <= 0f)
+        {
+            is_fire3_pressed = false;
+        }
+    }
 }
