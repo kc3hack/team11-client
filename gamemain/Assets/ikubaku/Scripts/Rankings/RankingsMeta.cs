6563e61387a750c0d9f90c8d2e99a51e4943603c
ikubaku <hide4d51@gmail.com>
2019-02-20 21:30:39 +0900

    Update server address

--- gamemain/Assets/ikubaku/Scripts/Rankings/RankingsMeta.cs
+++ gamemain/Assets/ikubaku/Scripts/Rankings/RankingsMeta.cs
@@ -10,7 +10,7 @@ namespace Assets.ikubaku.Scripts.Rankings
         public int current_page { get; set; }
         public string from { get; set; }
         public int last_page { get; set; }
-        public int path { get; set; }
+        public string path { get; set; }
         public int per_page { get; set; }
         public int to { get; set; }
         public int total { get; set; }
