5e172f42ed8488f9f37199c8b2672f6652f00a97
ikubaku <hide4d51@gmail.com>
2019-02-20 11:21:53 +0900

    Add 2P UI

---
+++ gamemain/Assets/ikubaku/Scripts/ButtonController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonController : MonoBehaviour, IPointerClickHandler {
+    public int NPersonnel = 1;
+    public Sprite SpriteNormal;
+    public Sprite SpriteSelected;
+    public Sprite SpriteDisabled;
+
+    private int index = 0;
+
+    private SpriteRenderer sr;
+
+    private InputController2P ic2;
+    private bool is_selected = false;
+    private bool is_on_cooldown = false;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ic2.SelectNPersonnel(NPersonnel, this);
+    }
+
+    // Use this for initialization
+    void Start () {
+        sr = GetComponent<SpriteRenderer>();
+
+        var im2 = GameObject.Find("2PInputManager");
+        ic2 = im2.GetComponent<InputController2P>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateAppearance();
+    }
+
+    void UpdateAppearance()
+    {
+        if(is_on_cooldown)
+        {
+            sr.sprite = SpriteDisabled;
+        } else if(is_selected)
+        {
+            sr.sprite = SpriteSelected;
+        } else
+        {
+            sr.sprite = SpriteNormal;
+        }
+    }
+
+    public void ChangeToSelected()
+    {
+        if (!is_on_cooldown)
+        {
+            is_selected = true;
+        }
+
+        //UpdateAppearance();
+    }
+
+    public void ChangeToNotSelected()
+    {
+        is_selected = false;
+
+        //UpdateAppearance();
+    }
+
+    public void ChangeToOnCooldown()
+    {
+        is_on_cooldown = true;
+        is_selected = false;
+
+        //UpdateAppearance();
+    }
+
+    public void ChangeToCooleddown()
+    {
+        is_on_cooldown = false;
+        is_selected = false;
+
+        //UpdateAppearance();
+    }
+
+    public void SetIndex(int i)
+    {
+        index = i;
+    }
+    public int GetIndex()
+    {
+        return index;
+    }
+}
---
+++ gamemain/Assets/ikubaku/Scripts/InputController2P.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController2P : MonoBehaviour {
+    public ButtonController[] Buttons;
+    public float[] CooldownCounts;
+
+    public UnityEngine.UI.Text debug_text;
+
+    private int n_personnel = 0;
+    private int selected_idx = -1;
+    private float[] cnt_cooldown;
+	// Use this for initialization
+	void Start () {
+        cnt_cooldown = new float[CooldownCounts.Length];
+        for(int i=0; i<Buttons.Length; i++)
+        {
+            Buttons[i].SetIndex(i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        debug_text.text = n_personnel.ToString();
+		for(int i=0; i<cnt_cooldown.Length; i++)
+        {
+            if(cnt_cooldown[i] > 0f)
+            {
+                cnt_cooldown[i] -= Time.deltaTime;
+                if(cnt_cooldown[i] <= 0f)
+                {
+                    Buttons[i].ChangeToCooleddown();
+                }
+            }
+        }
+	}
+
+    public void SelectNPersonnel(int n, ButtonController sender)
+    {
+        var idx = sender.GetIndex();
+        if(cnt_cooldown[idx] <= 0f)
+        {
+            if(selected_idx != -1)
+            {
+                Buttons[selected_idx].ChangeToNotSelected();
+            }
+            Buttons[idx].ChangeToSelected();
+            selected_idx = idx;
+
+            n_personnel = n;
+        }
+    }
+}
